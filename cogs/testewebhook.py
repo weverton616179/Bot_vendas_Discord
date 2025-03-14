6bfb06a819e7d138a6077e82284466c17dcd779f
Weverton vieira <weverton616179@gmail.com>
2025-03-14 10:37:07 -0300

    dasdkaoskdas

--- cogs/testewebhook.py
+++ cogs/testewebhook.py
@@ -10,7 +10,7 @@ class TesteWebhook(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         # self.app = Quart(__name__)
-        self.sdk = mercadopago.SDK("APP_USR-858971298465680-021921-8b0ac97868ffc64211357c5da2beb2fc-259696807")
+        # sself.sdk = mercadopago.SDK("APP_USR-858971298465680-021921-8b0ac97868ffc64211357c5da2beb2fc-259696807")
         # self.setup_routes()
         # self.bot.loop.create_task(self.run_quart())
 
